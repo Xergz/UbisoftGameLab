af57a5fec02d70b8af454865c6b756f58f03a4a3
Nathan Giraldeau <giranath@gmail.com>
2016-01-23 20:10:17 -0500

    Add a component to declare object as floating
    A floating gameobject will follow the height of waves.

---
+++ Assets/Scripts/Component/FloatOnWave.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatOnWave : MonoBehaviour {
+	[Tooltip("Specify the wave's manager")]
+	public WaveManager waves;
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = this.transform.position;
+		pos.y = waves.getOceanHeightAt (pos.x, pos.z);
+
+		this.transform.position = pos;
+	}
+}
