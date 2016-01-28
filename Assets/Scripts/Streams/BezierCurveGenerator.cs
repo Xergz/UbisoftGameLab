8dd3dc2ee31d696ea78c79e9b86087ae192eeea3
Gabriel Provost <gabriel.provost@hotmail.com>
2016-01-27 19:53:46 -0500

    Added bezier curves and basic line rendering

---
+++ Assets/Scripts/Streams/BezierCurveGenerator.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BezierCurveGenerator {
+
+	private int segmentCount;
+
+	private bool generationPending = false;
+
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private	Vector3 startTangent;
+	private Vector3 endTangent;
+
+	private Vector3[] bezierPoints;
+
+	public BezierCurveGenerator(int segmentCount) {
+		this.segmentCount = segmentCount;
+    }
+
+	public void SetHandles(Vector3 startPosition, Vector3 endPosition, Vector3 startTangent, Vector3 endTangent) {
+		this.startPosition = startPosition;
+		this.endPosition = endPosition;
+		this.startTangent = startTangent;
+		this.endTangent = endTangent;
+		generationPending = true;
+	}
+
+	public Vector3[] GenerateBezierCurve() {
+		if(generationPending) {
+			Debug.Log("Regenerating curve");
+
+			bezierPoints = new Vector3[segmentCount + 1];
+			bezierPoints[0] = startPosition;
+			bezierPoints[segmentCount] = endPosition;
+
+			float inc = 1.0F / segmentCount;
+			for(int i = 1; i < segmentCount; ++i) {
+				bezierPoints[i] = CalculateBezierPoint(i * inc);
+			}
+			generationPending = false;
+		}
+
+		return bezierPoints;
+	}
+
+
+	// Calculates a point at a fraction t along the curve
+	// [x,y,z]= (1–t)^3 * startPosition + 3 * (1–t)^2 * t * startTangent + 3 * (1–t) * t^2 * endTangent + t^3 * endPosition
+	private Vector3 CalculateBezierPoint(float t) {
+		float t2 = t * t;
+		float t3 = t2 * t;
+		float u = 1 - t;
+		float u2 = u * u;
+		float u3 = u2 * u;
+
+		return (u3 * startPosition) + (3 * u2 * t * startTangent) + (3 * u * t2 * endTangent) + (t3 * endPosition);
+	}
+
+
+	public void SetSegmentCount(int segmentCount) {
+		if(this.segmentCount != segmentCount) {
+			this.segmentCount = segmentCount;
+			generationPending = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Streams/Stream.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+
+public class Stream : MonoBehaviour {
+
+	public int segmentCount = 20;
+
+	public bool debug = false;
+
+	public Material debugMaterial;
+
+	public Vector3 startPosition;
+	public Vector3 endPosition;
+	public Vector3 startTangent;
+	public Vector3 endTangent;
+
+
+	private LineRenderer lineRenderer;
+
+	private Vector3[] streamCurve;
+
+	private BezierCurveGenerator curveGenerator; 
+
+	void Start () {
+		curveGenerator = new BezierCurveGenerator(segmentCount);
+		if(debug) {
+			lineRenderer = gameObject.AddComponent<LineRenderer>();
+			lineRenderer.SetWidth(0.05F, 0.05F);
+			lineRenderer.material = debugMaterial;
+			lineRenderer.shadowCastingMode = ShadowCastingMode.Off;
+			lineRenderer.receiveShadows = false;
+		}
+	}
+	
+	void Update () {
+		curveGenerator.SetSegmentCount(segmentCount);
+		curveGenerator.SetHandles(startPosition, endPosition, startTangent, endTangent);
+		streamCurve = curveGenerator.GenerateBezierCurve();
+
+		if(debug) {
+			lineRenderer.SetVertexCount(segmentCount + 1);
+			lineRenderer.SetPositions(streamCurve);
+		}
+	}
+}
