7ba04dbd54e9287870b7cebc55b7a1b43a5ed2cf
Nathan Giraldeau <giranath@gmail.com>
2016-02-02 18:55:12 -0500

    Define the checkpoint's data
    The struct called Checkpoint holds all the required informations about a
    checkpoint.

---
+++ Assets/Scripts/Managers/Checkpoints/Checkpoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Store every data about a checkpoint
+/// </summary>
+public struct Checkpoint {
+
+	/// <summary>
+	/// The scene where is located the checkpoint
+	/// </summary>
+	int 	SceneID;
+
+	/// <summary>
+	/// The player's life
+	/// </summary>
+	int 	CurrentLife;
+
+	/// <summary>
+	/// Position from the top where the player should start
+	/// </summary>
+	Vector2 Position;
+
+	/// <summary>
+	/// The orientation from the top from which the player should start
+	/// </summary>
+	float 	Orientation;
+
+	/// <summary>
+	/// Enumerates every collectable the player may obtains
+	/// </summary>
+	/// <remarks>Must contains every collectables, even the ones the player didn't find</remarks>
+	Dictionary<int, bool> Collectables;
+}
