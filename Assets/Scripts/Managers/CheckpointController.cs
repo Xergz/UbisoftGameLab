3095d5f67d96b1ea0ec3ceec4872d0e9df6de74b
Nathan Giraldeau <giranath@gmail.com>
2016-02-02 22:08:47 -0500

    Add model's layer for checkpoints

---
+++ Assets/Scripts/Managers/CheckpointController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+/// <summary>
+/// Handles checkpoints
+/// </summary>
+public class CheckpointController {
+	private GameRestorer restorer;
+	private CheckpointModel model;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CheckpointController"/> class.
+	/// </summary>
+	/// <param name="restorer">The assigned game restorer.</param>
+	public CheckpointController(GameRestorer restorer) {
+		this.restorer = restorer;
+	}
+
+	/// <summary>
+	/// Restore the game from a checkpoint
+	/// </summary>
+	/// <returns><c>true</c>, if the game was restored, <c>false</c> otherwise.</returns>
+	/// <param name="checkpoint">The checkpoint to restore from.</param>
+	public bool RestoreFromLastCheckpoint() {
+		bool restored = false;
+
+		try {
+			Checkpoint checkpoint = this.model.Current;
+
+			this.restorer.RestoreGameStateFrom(checkpoint);
+
+			restored = true;
+		}
+		catch(Exception e) {
+			// Restoration failed
+			// TODO: Determine what to do in case of failure
+			Debug.LogError(e.Message);
+		}
+
+		return restored;
+	}
+
+	/// <summary>
+	/// Discards the last checkpoint.
+	/// </summary>
+	/// <remarks>The last checkpoint will be replaced by the one before it</remarks>
+	public void DiscardLastCheckpoint() {
+		this.model.Discard ();
+	}
+}
---
+++ Assets/Scripts/Managers/Checkpoints/CheckpointModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// This class is the data representation of the checkpoints
+/// </summary>
+public class CheckpointModel {
+	private Stack<Checkpoint> checkpoints;
+	private string FILE_EXTENSION = "chk";
+	private byte[] VALID_HEADER_MAGIC = { 67, 72, 69,  75};
+	private byte   VALID_HEADER_VERSION = 10;
+
+	/// <summary>
+	/// Describe the file header
+	/// </summary>
+	private struct FileHeader {
+		public byte[] Magic;
+		public byte Version;
+	}
+
+	/// <summary>
+	/// Get the current checkpoint
+	/// </summary>
+	/// <value>The last.</value>
+	public Checkpoint Current {
+		get {
+			return checkpoints.Peek ();
+		}
+	}
+
+	/// <summary>
+	/// Discard the current checkpoint
+	/// </summary>
+	public void Discard() {
+		checkpoints.Pop ();
+	}
+
+	/// <summary>
+	/// Update the checkpoint
+	/// </summary>
+	/// <param name="newCheckpoint">The new checkpoint.</param>
+	public void Update(Checkpoint newCheckpoint) {
+		checkpoints.Push (newCheckpoint);
+	}
+
+	/// <summary>
+	/// Save the model into a permanent representation
+	/// </summary>
+	/// <param name="file">The file where to save the model.</param>
+	public void SaveTo(string file) {
+		// Create a temporary file where the data will be saved
+		// A temporary file is used to prevent save corruption,
+		// In a case where a corruption happened, the original file 
+		// will still be usable
+		string tempFile = Path.GetTempFileName();
+		Stack<Checkpoint> lastCheckpoints = new Stack<Checkpoint> (this.checkpoints);
+
+		using (BinaryWriter writer = new BinaryWriter(File.Open(tempFile, FileMode.Open))) {
+			// Write the file header
+			for (int i = 0; i < 4; i++) {
+				writer.Write (VALID_HEADER_MAGIC [i]);
+			}
+			writer.Write (VALID_HEADER_VERSION);
+
+			// Write
+		}
+			
+		// TODO: Replace the current save with tempFile
+	}
+
+	/// <summary>
+	/// Load the model from a permanent representation
+	/// </summary>
+	/// <param name="file">The file from where to load the model.</param>
+	public void LoadFrom(string file) {
+	}
+}
