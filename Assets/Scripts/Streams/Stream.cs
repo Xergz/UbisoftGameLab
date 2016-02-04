9d62cc92d48e954e85bdaf617c4e0facc61c32b7
Gabriel Provost <gabriel.provost@hotmail.com>
2016-02-03 21:17:06 -0500

    Added stream oscillation

--- Assets/Scripts/Streams/Stream.cs
+++ Assets/Scripts/Streams/Stream.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 using System.Collections;
+using SimplexNoise;
 
 /// <summary>
 /// Describes a stream
@@ -13,14 +14,11 @@ public class Stream : MonoBehaviour {
 	private const int MIN_SEGMENT_COUNT = 10; // Minimum number of segments in the bezier curve
 
 	private const float MAX_CURVE_POINTS_SPACING = 2F; // Maximum distance between 2 points of the curve
-    private const float MAX_AMPLITUDE = 3F; // The maximum amplitude for the natural oscillation of the curve
-    private const float MIN_AMPLITUDE = 0.25F; // The minimum amplitude for the natural oscillation of the curve
+    private const float MAX_POSITION_AMPLITUDE = 1.75F; // The maximum amplitude for the position oscillation
+    private const float MIN_POSITION_AMPLITUDE = 0.25F; // The minimum amplitude for the position oscillation 
+	private const float MAX_TANGENT_AMPLITUDE = 25F; // The maximum amplitude for the tangent oscillation
+	private const float MIN_TANGENT_AMPLITUDE = 5F; // The minimum amplitude for the tangent oscillation
     private const float RANDOMIZATION_TIME = 30F; // The number of seconds between each randomization of the curve amplitude
-
-    private float amplitudeStartPos = 1; // The amplitude of the oscillation for the starting position
-    private float amplitudeStartTan = 1; // The amplitude of the oscillation for the starting tangent
-    private float amplitudeEndPos = 1; // The amplitude of the oscillation for the ending position
-    private float amplitudeEndTan = 1; // The amplitude of the oscillation for the ending tangent
 
     [Tooltip("The width of the stream")]
     [SerializeField]
@@ -28,8 +26,18 @@ public class Stream : MonoBehaviour {
     [Tooltip("The strength of the stream")]
     [SerializeField]
     private float strength = 5F;
-    // To know when the width changes (needed since I can't do a function if I want to allow in editor modification)
+    // To know when the width changes (needed since I can't do a function if I want to allow in editor modification) I'm open to suggestions
     private float oldWidth;
+	[Tooltip("The speed at which the stream will oscillate")]
+	[SerializeField]
+	private float oscillationSpeed = 0.25F;
+	[Tooltip("The amplitude at which the stream position handles will oscillate in game units")]
+	[SerializeField]
+	private float positionOscillationAmplitude = 1F;
+	[Tooltip("The amplitude at which the stream tangent handles will oscillate in degrees")]
+	[SerializeField]
+	private float tangentOscillationAmplitude = 15F;
+
 
     [Tooltip("The color of the stream")]
     [SerializeField]
@@ -38,7 +46,7 @@ public class Stream : MonoBehaviour {
     [Tooltip("The direction of the stream")]
     [SerializeField]
     private EnumStreamDirection direction = EnumStreamDirection.POSITIVE;
-    // To know when the direction changes (needed since I can't do a function if I want to allow in editor modification)
+    // To know when the direction changes (needed since I can't do a function if I want to allow in editor modification) I'm open to suggestions
     private EnumStreamDirection oldDirection;
 
     [Tooltip("Displays the starting and ending tangents as well as the curve itself")]
@@ -52,16 +60,16 @@ public class Stream : MonoBehaviour {
 
     [Tooltip("Starting position of the bezier curve representing the stream")]
     [SerializeField]
-    private Vector3 startPosition;
+    private Vector3 startPositionHandle;
     [Tooltip("Starting tangent of the bezier curve representing the stream")]
     [SerializeField]
-    private Vector3 startTangent;
+    private Vector3 startTangentHandle;
     [Tooltip("Ending position of the bezier curve representing the stream")]
     [SerializeField]
-    private Vector3 endPosition;
+    private Vector3 endPositionHandle;
     [Tooltip("Ending tangent of the bezier curve representing the stream")]
     [SerializeField]
-    private Vector3 endTangent;
+    private Vector3 endTangentHandle;
 
     private Vector3[] streamCurve; // The bezier curve
 	private Vector3[] tangents; // The array containing the tangents for each point of the stream
@@ -75,16 +83,20 @@ public class Stream : MonoBehaviour {
 
 	private Mesh mesh; // The curve's mesh
 
+	[Tooltip("The material to apply to a green stream")]
     [SerializeField]
     private Material greenStreamMaterial;
+	[Tooltip("The material to apply to a blue stream")]
     [SerializeField]
     private Material blueStreamMaterial;
+	[Tooltip("The material to apply to a yellow stream")]
     [SerializeField]
     private Material yellowStreamMaterial;
+	[Tooltip("The material to apply to a red stream")]
     [SerializeField]
     private Material redStreamMaterial;
 
-    [Tooltip("A prefab used to represent the direction of the tangents")]
+    [Tooltip("A prefab used to visually represent the direction of the tangents")]
     [SerializeField]
     private GameObject tangentArrow;
 
@@ -92,7 +104,7 @@ public class Stream : MonoBehaviour {
 
 
     /// <summary>
-    /// Get the force to apply to an object within the stream (assumes the object is within the stream)
+    /// Get the force to apply to an object within the stream (assumes the object is within the stream).
     /// </summary>
     /// <param name="position">The position of the object</param>
     /// <returns>The force to apply to the object</returns>
@@ -101,28 +113,28 @@ public class Stream : MonoBehaviour {
 	}
 
     /// <summary>
-    /// Switch the direction of the stream (POSITIVE -> NEGATIVE or NEGATIVE -> POSITIVE)
+    /// Switch the direction of the stream (POSITIVE -> NEGATIVE or NEGATIVE -> POSITIVE).
     /// </summary>
     public void SwitchDirection() {
         direction = (EnumStreamDirection) (-((int) direction));
     }
 
     /// <summary>
-    /// Sets the handles of the bezier curve representing the stream (positions and tangents)
+    /// Sets the handles of the bezier curve representing the stream (positions and tangents) Might be change to prevent modifiying some values.
     /// </summary>
-    /// <param name="startPosition">Starting position of the bezier curve representing the stream</param>
-    /// <param name="startTangent">Starting tangent of the bezier curve representing the stream</param>
-    /// <param name="endPosition">Ending position of the bezier curve representing the stream</param>
-    /// <param name="endTangent">Ending tangent of the bezier curve representing the stream</param>
-    public void SetHandles(Vector3 startPosition, Vector3 startTangent, Vector3 endPosition, Vector3 endTangent) {
-        this.startPosition = startPosition;
-        this.startTangent = startTangent;
-        this.endPosition = endPosition;
-        this.endTangent = endTangent;
+    /// <param name="startPositionHandle">Starting position of the bezier curve representing the stream</param>
+    /// <param name="startTangentHandle">Starting tangent of the bezier curve representing the stream</param>
+    /// <param name="endPositionHandle">Ending position of the bezier curve representing the stream</param>
+    /// <param name="endTangentHandle">Ending tangent of the bezier curve representing the stream</param>
+	public void SetHandles(Vector3 startPositionHandle, Vector3 startTangentHandle, Vector3 endPositionHandle, Vector3 endTangentHandle) {
+		this.startPositionHandle = startPositionHandle;
+		this.startTangentHandle = startTangentHandle;
+		this.endPositionHandle = endPositionHandle;
+		this.endTangentHandle = endTangentHandle;
     }
 
 
-	private void Start () {
+	private void Awake () {
 		curveGenerator = new BezierCurveGenerator();
 
 		mesh = new Mesh();
@@ -148,7 +160,7 @@ public class Stream : MonoBehaviour {
         oldDirection = (EnumStreamDirection) (-((int) direction));
 
         // Invoke the RandomizeAmplitude method every RANDOMIZATION_TIME
-        InvokeRepeating("RandomizeAmplitude", RANDOMIZATION_TIME, RANDOMIZATION_TIME);
+        //InvokeRepeating("RandomizeAmplitude", RANDOMIZATION_TIME, RANDOMIZATION_TIME);
 
         #region DEBUG
         curveLineRenderer = transform.GetChild(1).GetComponent<LineRenderer>();
@@ -158,8 +170,9 @@ public class Stream : MonoBehaviour {
 	}
 	
 	private void Update () {
-        //UpdateHandles();
-		UpdateCurve();
+		Vector3 startPosition, startTangent, endPosition, endTangent; // out parameters
+		UpdateHandles(out startPosition, out startTangent, out endPosition, out endTangent);
+		UpdateCurve(startPosition, startTangent, endPosition, endTangent);
 
 		#region DEBUG
 		if(debug) {
@@ -187,12 +200,50 @@ public class Stream : MonoBehaviour {
 		#endregion
 	}
 
+	/// <summary>
+	/// Update the handles of the curve in order to make them oscillate according to a Perlin noise. 
+	/// Position handles oscillate on an axis perpendicular to their tangent.
+	/// Tangent handles oscillate on a circular axis around the position handles.
+	/// </summary>
+	/// <param name="startPosition">Out: The displaced starting position</param>
+	/// <param name="startTangent">Out: The displaced starting tangent</param>
+	/// <param name="endPosition">Out: The displaced ending position</param>
+	/// <param name="endTangent">Out: The displaced ending tangent</param>
+	private void UpdateHandles(out Vector3 startPosition, out Vector3 startTangent, out Vector3 endPosition, out Vector3 endTangent) {
+		// Get the noise to modify handles' positions
+		float startNoise = Noise.Generate(1, Time.time * oscillationSpeed, 1); // Using 3 parameters since the function with 2 is buggy
+		float endNoise = Noise.Generate(segmentCount + 2, Time.time * oscillationSpeed, 1); // Using 3 parameters since the function with 2 is buggy
+
+		// Calculate tangents related to original handles (without the oscillation) since the oscillation will be made according to this
+		Quaternion rotation = Quaternion.AngleAxis(-90, Vector3.up);
+		Vector3 rotatedStartTangent = rotation * Vector3.Normalize(startTangentHandle - startPositionHandle);
+		Vector3 rotatedEndTangent = rotation * Vector3.Normalize(-(endTangentHandle - endPositionHandle));
+
+		// Move the positions along a vector perpendicular to their tangent
+		startPosition = startPositionHandle + (rotatedStartTangent * startNoise * positionOscillationAmplitude);
+		endPosition = endPositionHandle + (rotatedEndTangent * endNoise * positionOscillationAmplitude);
+
+		// Rotate the tangents around these new positions
+		// Therefore move them along with the positions then rotate them around the new positions
+		Quaternion startTangentRotation = Quaternion.AngleAxis(startNoise * tangentOscillationAmplitude, Vector3.up);
+		Quaternion endTangentRotation = Quaternion.AngleAxis(endNoise * tangentOscillationAmplitude, Vector3.up);
+		startTangent = (startTangentHandle + (startPositionHandle - startPosition)); // New start tangent position before rotation
+		startTangent = (startTangentRotation * (startTangent - startPosition)) + startPosition; // Start tangent rotated
+		endTangent = (endTangentHandle + (endPositionHandle - endPosition)); // New end tangent position before rotation
+		endTangent = (endTangentRotation * (endTangent - endPosition)) + endPosition; // End tangent rotated
+	}
+
     /// <summary>
     /// Update the stream's bezier curve. Will do nothing if the stream hasn't changed. 
+	/// Most of the time the stream has changed due to oscillation.
     /// Can be safely called every frame without impact on performance. 
     /// Also makes sure the curve is smooth enough by calculated the required segment count.
     /// </summary>
-    private void UpdateCurve() {
+	/// <param name="startPosition">Starting position of the bezier curve representing the stream</param>
+	/// <param name="startTangent">Starting tangent of the bezier curve representing the stream</param>
+	/// <param name="endPosition">Ending position of the bezier curve representing the stream</param>
+	/// <param name="endTangent">Ending tangent of the bezier curve representing the stream</param>
+	private void UpdateCurve(Vector3 startPosition, Vector3 startTangent, Vector3 endPosition, Vector3 endTangent) {
 		// Only update the curve if a new one was generated
 		segmentCount = Mathf.Max(Mathf.Min(segmentCount, MAX_SEGMENT_COUNT), MIN_SEGMENT_COUNT);
 		bool curveChanged = curveGenerator.GenerateBezierCurve(startPosition, startTangent, endPosition, endTangent, segmentCount, out streamCurve);
@@ -204,7 +255,7 @@ public class Stream : MonoBehaviour {
 				curveGenerator.GenerateBezierCurve(startPosition, startTangent, endPosition, endTangent, segmentCount, out streamCurve);
 			}
 
-            GenerateTangents();
+			GenerateTangents(startPosition, startTangent, endPosition, endTangent);
 		}
 
 		if(curveChanged || oldDirection != direction) {
@@ -218,8 +269,25 @@ public class Stream : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Generate new tangents from the curve.
+	/// </summary>
+	/// <param name="startPosition">Starting position of the bezier curve representing the stream</param>
+	/// <param name="startTangent">Starting tangent of the bezier curve representing the stream</param>
+	/// <param name="endPosition">Ending position of the bezier curve representing the stream</param>
+	/// <param name="endTangent">Ending tangent of the bezier curve representing the stream</param>
+	private void GenerateTangents(Vector3 startPosition, Vector3 startTangent, Vector3 endPosition, Vector3 endTangent) {
+		tangents = new Vector3[streamCurve.Length];
+		tangents[0] = Vector3.Normalize(startTangent - startPosition);
+		tangents[streamCurve.Length - 1] = Vector3.Normalize(-(endTangent - endPosition));
+
+		for(int i = 1; i < streamCurve.Length - 1; ++i) {
+			tangents[i] = Vector3.Normalize(streamCurve[i + 1] - streamCurve[i - 1]);
+		}
+	}
+
     /// <summary>
-    /// Generates new arrows to visualize the tangents
+    /// Generate new arrows to visualize the tangents.
     /// </summary>
     private void GenerateTangentArrows() {
         // Clear the old arrows
@@ -236,22 +304,8 @@ public class Stream : MonoBehaviour {
         }
     }
 
-    /// <summary>
-    /// Generates new tangents from the curve
-    /// </summary>
-    private void GenerateTangents() {
-        tangents = new Vector3[streamCurve.Length];
-        tangents[0] = Vector3.Normalize(startTangent - startPosition);
-        tangents[streamCurve.Length - 1] = Vector3.Normalize(-(endTangent - endPosition));
-
-        for (int i = 1; i < streamCurve.Length - 1; ++i)
-        {
-            tangents[i] = Vector3.Normalize(streamCurve[i + 1] - streamCurve[i - 1]);
-        }
-    }
-
 	/// <summary>
-    /// Generate the stream's mesh (vertices, UV coordinates, normals and triangles)
+    /// Generate the stream's mesh (vertices, UV coordinates, normals and triangles).
     /// </summary>
 	private void GenerateMesh() {
 		Quaternion rotation = Quaternion.AngleAxis(-90, Vector3.up);
@@ -266,8 +320,8 @@ public class Stream : MonoBehaviour {
 			int i2 = i * 2;
 			Vector3 rotatedTangent = rotation * tangents[i];
 
-			vertices[i2] = streamCurve[i] + rotatedTangent * (width / 2);
-			vertices[i2 + 1] = streamCurve[i] - rotatedTangent * (width / 2);
+			vertices[i2] = streamCurve[i] + (rotatedTangent * (width / 2));
+			vertices[i2 + 1] = streamCurve[i] - (rotatedTangent * (width / 2));
 
 			uv[i2] = new Vector2(vertices[i2].x, vertices[i2].z);
 			uv[i2 + 1] = new Vector2(vertices[i2 + 1].x, vertices[i2 + 1].z);
@@ -296,7 +350,7 @@ public class Stream : MonoBehaviour {
 	}
 
 	/// <summary>
-    /// Gets the closest curve point to a position in the world
+    /// Get the closest curve point to a position in the world.
     /// </summary>
 	private int GetClosestCurvePointIndex(Vector3 position) {
 		int closestPoint = 0;
@@ -315,10 +369,11 @@ public class Stream : MonoBehaviour {
 		return closestPoint;
 	}
 
+	/// <summary>
+	/// Randomize the oscillation amplitudes for the positions and the tangents.
+	/// </summary>
     private void RandomizeAmplitude() {
-        amplitudeStartPos = Random.Range(MIN_AMPLITUDE, MAX_AMPLITUDE);
-        amplitudeStartTan = Random.Range(MIN_AMPLITUDE, MAX_AMPLITUDE);
-        amplitudeEndPos = Random.Range(MIN_AMPLITUDE, MAX_AMPLITUDE);
-        amplitudeEndTan = Random.Range(MIN_AMPLITUDE, MAX_AMPLITUDE);
+        positionOscillationAmplitude = Random.Range(MIN_POSITION_AMPLITUDE, MAX_POSITION_AMPLITUDE);
+		tangentOscillationAmplitude = Random.Range(MIN_TANGENT_AMPLITUDE, MAX_TANGENT_AMPLITUDE);
     }
 }
