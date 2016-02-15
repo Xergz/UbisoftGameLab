cece2329a31c191de3b65c5b7c3227323c4f0f81
Alexandre Lemay <lemay.alex95@gmail.com>
2016-02-15 12:33:56 -0500

    Ajout des classe pour les power avec un power controller qui va appeler le pouvoir qu'on lui envoie

---
+++ Assets/Scripts/Powers/PowerChangeDirection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+    /*classe Power qui change de direction, override la fonction activatePower pour que ca change la direction de la liste qui lui ait donner
+      ce diferencie grace au enumPower
+    */
+public class PowerChangeDirection : Power {
+
+	// Use this for initialization
+	void Start () {
+        powerType = enumPower.ChangeDirection;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        cooldownTimer();
+    }
+   
+    //fonction qui fais le pouvoir selon la liste de stream qui lui ait envoyer
+    public override void activatePower(List<Stream> liste)
+    {
+        liste.ForEach((stream) =>
+        {
+            stream.SwitchDirection();
+        });
+
+        }
+
+}
+
---
+++ Assets/Scripts/Powers/PowerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerController : MonoBehaviour {
+
+
+    List<Power> powers = new List<Power>();
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool isPowerReady(enumPower power)
+    {
+        for(int i=0; i < powers.Count; i++)
+        {
+            if (powers[i].getPowerType() == power)
+            {
+                return powers[i].ispowerReady();
+            }
+        }
+        return false;
+    }
+    public bool activatePower(enumPower power, List<Stream> listStream)
+    {
+        for(int i=0; i < powers.Count; i++)
+        {
+            
+            if (powers[i].getPowerType() == power)
+            {
+                if (powers[i].ispowerReady())
+                {
+                    powers[i].activatePower(listStream);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+}
