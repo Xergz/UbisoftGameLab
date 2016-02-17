3fab91bc38702ca84011a861fbd15056dad2bad0
Gabriel Provost <gabriel.provost@hotmail.com>
2016-02-16 22:19:30 -0500

    Missing commit

--- Assets/Scripts/Powers/PowerEnum.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-
-//enum de tout les type de pourvoir (la y'en a un mais c'est si jamais on en met plus
-    public enum enumPower { ChangeDirection};
