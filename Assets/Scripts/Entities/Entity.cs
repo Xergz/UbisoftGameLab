698b2f9fe115b9e5ab79412972f97572d25dc68a
Emile Fugulin <emilefugulin@hotmail.com>
2016-02-21 15:13:55 -0500

    Add entity system

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Entity : MonoBehaviour {
+    public abstract void ReceiveHit();
+    protected abstract void OnTriggerEnter(Collider other);
+    protected abstract void OnTriggerStay(Collider other);
+}
--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : Entity {
 	public PlayerController PlayerController { get; set; } // The PlayerController linked to this player
 
-	// This function will probably move partially to the parent class. Probably as a template method so that all entities are affected by streams.
-	// Will wait until one shade is done though since I am still not sure of the right implementation.
-	private void OnTriggerEnter(Collider other) {
+    public override void ReceiveHit() {
+        PlayerController.DamagePlayer(1);
+    }
+
+    // This function will probably move partially to the parent class. Probably as a template method so that all entities are affected by streams.
+    // Will wait until one shade is done though since I am still not sure of the right implementation.
+    protected override void OnTriggerEnter(Collider other) {
 		if(other.gameObject.CompareTag("Fragment")) {
 			other.gameObject.SetActive(false);
 			PlayerController.AddFragment(other.GetComponent<Fragment>());
-		} else if(other.gameObject.CompareTag("Stream")) {
-			PlayerController.AddForce(other.gameObject.GetComponent<Stream>().GetForceAtPosition(transform.position));
 		}
 	}
+
+    protected override void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("Stream")) {
+            PlayerController.AddForce(other.gameObject.GetComponent<Stream>().GetForceAtPosition(transform.position));
+        }
+    }
 }
