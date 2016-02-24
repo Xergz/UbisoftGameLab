98cb89834833c14ad25aad57d453c88de1835a6e
Gabriel Provost <gabriel.provost@hotmail.com>
2016-02-23 23:24:27 -0500

    Added a bunch of custom actions for the ramming fist

---
+++ Assets/AI/Motor/RammingEnemyMotor.cs
@@ -0,0 +1,28 @@
+﻿using RAIN.Motion;
+using RAIN.Representation;
+using RAIN.Serialization;
+using System;
+using UnityEngine;
+
+[RAINSerializableClass]
+public class RammingEnemyMotor : UnityNavMeshMotor {
+
+	private RammingEntity rammingEntity;
+
+	public override void Start() {
+		base.Start();
+
+		rammingEntity = AI.Body.GetComponent<RammingEntity>();
+	}
+
+	public override bool Move() {
+		// Set the area costs
+		if(rammingEntity.IsRamming) {
+			// Set all costs to ocean cost
+		} else {
+			// Set the costs according to target
+		}
+
+		return base.Move();
+	}
+}
