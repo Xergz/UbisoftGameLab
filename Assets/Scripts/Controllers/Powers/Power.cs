0f90869eea8b4f8205471d01f80b82202f9c35a1
Alexandre Lemay <lemay.alex95@gmail.com>
2016-03-16 11:47:47 -0400

    power + power controller conflict fix

--- Assets/Scripts/Controllers/Powers/Power.cs
+++ Assets/Scripts/Controllers/Powers/Power.cs
@@ -1,87 +1,94 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class Power : MonoBehaviour {
+public abstract class Power : MonoBehaviour
+{
 
-	public bool IsReady { get { return ready; } }
+    public bool IsReady { get { return ready; } }
 
-	public EnumPower PowerType { get; protected set; }
+    public EnumPower PowerType { get; protected set; }
 
     public Image CooldownBar;
 
     [Tooltip("The cooldown time for this power")]
-	[SerializeField]
-	protected float cooldownTime = 3.0F;
-	[Tooltip("This should not be changed and is for debugging purposes only")]
-	[SerializeField]
-	protected float elapsedTime = 0; // The elapsed time since the power has been used
-	protected float cooldownMultiplier = 1F; // The multiplier for the cooldown time
+    [SerializeField]
+    protected float cooldownTime = 3.0F;
+    [Tooltip("This should not be changed and is for debugging purposes only")]
+    [SerializeField]
+    protected float elapsedTime = 0; // The elapsed time since the power has been used
+    protected float cooldownMultiplier = 1F; // The multiplier for the cooldown time
 
-	protected bool ready = true;
+    protected bool ready = true;
 
-	protected Color cooldownColor = Color.white;
+    protected Color cooldownColor = Color.white;
 
-	/// <summary>
-	/// Activates the power
-	/// </summary>
-	 public virtual void Activate() {
-		if(ready) {
-			ExecuteAction();
-			ready = false;
-		}
-	}
+    /// <summary>
+    /// Activates the power
+    /// </summary>
+    public virtual void Activate()
+    {
+        if (ready)
+        {
+            ExecuteAction();
+            ready = false;
+        }
+    }
 
-	/// <summary>
-	/// Get the time left to the cooldown
-	/// </summary>
-	/// <returns></returns>
-	public float GetTimeLeftToCooldown() {
-		return (cooldownTime * cooldownMultiplier) - elapsedTime;
-	}
+    /// <summary>
+    /// Get the time left to the cooldown
+    /// </summary>
+    /// <returns></returns>
+    public float GetTimeLeftToCooldown()
+    {
+        return (cooldownTime * cooldownMultiplier) - elapsedTime;
+    }
 
-	public void SetCooldownMultiplier(float multiplier) {
-		cooldownMultiplier = multiplier;
-	}
-
-
-	/// <summary>
-	/// Executes the power
-	/// </summary>
-	protected abstract void ExecuteAction();
+    public void SetCooldownMultiplier(float multiplier)
+    {
+        cooldownMultiplier = multiplier;
+    }
 
 
-	/// <summary>
-	/// Update the elapsed time until it reaches the cooldown time
-	/// </summary>
-	private void UpdateCooldown() {
-		if(!ready) {
-			float timeLeft = GetTimeLeftToCooldown();
-			if(timeLeft > 0.01) {
-				elapsedTime += Time.deltaTime;
-				CooldownBar.color = cooldownColor;
-				CooldownBar.fillAmount = (timeLeft / (cooldownTime * cooldownMultiplier));			
-            } else {
-				elapsedTime = 0F;
-				ready = true;
+    /// <summary>
+    /// Executes the power
+    /// </summary>
+    protected abstract void ExecuteAction();
+
+
+    /// <summary>
+    /// Update the elapsed time until it reaches the cooldown time
+    /// </summary>
+    private void UpdateCooldown()
+    {
+        if (!ready)
+        {
+            float timeLeft = GetTimeLeftToCooldown();
+            if (timeLeft > 0.01)
+            {
+                elapsedTime += Time.deltaTime;
+                CooldownBar.color = cooldownColor;
+                CooldownBar.fillAmount = (timeLeft / (cooldownTime * cooldownMultiplier));
+            }
+            else {
+                elapsedTime = 0F;
+                ready = true;
                 CooldownBar.fillAmount = 0;
-				CooldownBar.color = Color.white;
+                CooldownBar.color = Color.white;
             }
         }
-	}
-
-	private void Start() {
-		if(Application.isPlaying) {
-			PowerController.RegisterPower(this); // We must wait for when the PowerController will be initialized so we use Start
-			CooldownBar.fillAmount = 0;
-			CooldownBar.color = Color.white;
-		}
     }
 
-<<<<<<< HEAD
-private void Update() {
-		UpdateCooldown();
-        cooldownTime = 3.0F - nbFragments * 0.1F;
+    private void Start()
+    {
+        if (Application.isPlaying)
+        {
+            PowerController.RegisterPower(this); // We must wait for when the PowerController will be initialized so we use Start
+            CooldownBar.fillAmount = 0;
+            CooldownBar.color = Color.white;
+        }
     }
+
+
 
     public void resetTimer()
     {
@@ -89,14 +96,11 @@ private void Update() {
         elapsedTime = 0;
     }
 
+    private void Update()
+    {
+        UpdateCooldown();
+    }
 }
-=======
-	private void Update() {
-			UpdateCooldown();
-		}
-	}
-
->>>>>>> origin/master
 
 
 
