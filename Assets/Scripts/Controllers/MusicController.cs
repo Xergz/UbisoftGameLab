943c2a2008f499d578f05cbf997ec92d8eda2c0a
Nathan Giraldeau <giranath@gmail.com>
2016-03-19 17:03:35 -0400

    The music now changes when entering a level

--- Assets/Scripts/Controllers/MusicController.cs
+++ Assets/Scripts/Controllers/MusicController.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MusicController : MonoBehaviour {
 	public AudioMixerSnapshot ChasedInSnapshot;
 	public AudioMixerSnapshot ChasedOutSnapshot;
+	public AudioClip OpenWorldClip;
+	public AudioClip LevelClip;
+
+
+	public AudioSource MainTrackSource;
+	public AudioSource SwapTrackSource;
 
 	public float bpm = 128;
 
@@ -12,14 +19,8 @@ public class MusicController : MonoBehaviour {
 	private float m_TransitionOut;
 	private float m_QuarterNote;
 	private int m_ChasingEnnemiesCount;
-
-	/// <summary>
-	/// Called when the zone changed
-	/// </summary>
-	/// <param name="newZone">The new zone where the player entered.</param>
-	public void OnZoneChanged(EnumZone newZone) {
-		// TODO: Fade audio clip
-	}
+	private bool m_Fade;
+	private float m_FadeAcc;
 
 	public void NewEnnemyStartedChasing() {
 		m_ChasingEnnemiesCount++;
@@ -39,6 +40,54 @@ public class MusicController : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Called when the zone changed
+	/// </summary>
+	/// <param name="newZone">The new zone where the player entered.</param>
+	public void OnZoneChanged(EnumZone newZone) {
+		switch (newZone) {
+		case EnumZone.OPEN_WORLD:
+			TargetClip (OpenWorldClip);
+			break;
+		case EnumZone.LEVEL_1:
+			TargetClip (LevelClip);
+			break;
+		case EnumZone.LEVEL_2:
+			TargetClip (LevelClip);
+			break;
+		case EnumZone.LEVEL_3:
+			TargetClip (LevelClip);
+			break;
+		case EnumZone.LEVEL_4:
+			TargetClip (LevelClip);
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void TargetClip(AudioClip target) {
+		SwapTrackSource.volume = 0;
+		SwapTrackSource.clip = target;
+		SwapTrackSource.Play ();
+
+		MainTrackSource.volume = 1;
+		m_Fade = true;
+
+		m_FadeAcc = 0;
+	}
+
+	private void SwapTracks() {
+		AudioSource temp = MainTrackSource;
+		MainTrackSource = SwapTrackSource;
+		SwapTrackSource = temp;
+
+		MainTrackSource.volume = 1;
+		SwapTrackSource.volume = 0;
+
+		m_Fade = false;
+	}
+
 	// UNITY CALLBACKS
 	void Start() {
 		m_QuarterNote = 60 / bpm;
@@ -46,14 +95,30 @@ public class MusicController : MonoBehaviour {
 		m_TransitionOut = m_QuarterNote * 2;// * 16;
 
 		m_ChasingEnnemiesCount = 0;
+		m_Fade = false;
 	}
 
 	void Update() {
+		// TODO: Remove these
 		if (Input.GetKeyDown ("p")) {
 			NewEnnemyStartedChasing ();
 		}
 		if (Input.GetKeyDown ("l")) {
 			EnnemyStoppedChasing ();
+		}
+
+		float FadeDuration = 10.0f;
+
+		// Fade musics
+		if (m_Fade) {
+			SwapTrackSource.volume = Mathf.Lerp (0, 1, m_FadeAcc / FadeDuration);
+			MainTrackSource.volume = Mathf.Lerp (1, 0, m_FadeAcc / FadeDuration);
+
+			if (m_FadeAcc >= FadeDuration) {
+				SwapTracks ();
+			}
+
+			m_FadeAcc += Time.deltaTime;
 		}
 	}
 }
