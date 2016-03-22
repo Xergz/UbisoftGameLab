eaffe45e823dbd3cf9b5f659b83a1988dc6f8315
Comeau <alexandre.comeau-vermeersch@hotmail.com>
2016-03-21 22:22:48 -0400

    added some other sounds, resized collision vfx

--- Assets/Scripts/Entities/Enemies/AudioController.cs
+++ Assets/Scripts/Entities/Enemies/AudioController.cs
@@ -11,7 +11,11 @@ public class AudioController : MonoBehaviour {
         receiveHit,
         enterOpenWorld,
         collectFragment,
-        death
+        death,
+        collectLife,
+        collision,
+        useBoost,
+        reverseStream
     }
 
 	public List<AudioClip> clipsFar = new List<AudioClip>();
@@ -21,43 +25,32 @@ public class AudioController : MonoBehaviour {
     public List<AudioClip> clipsEnterOpenWorld = new List<AudioClip>();
     public List<AudioClip> clipsCollectFragments = new List<AudioClip>();
     public List<AudioClip> clipsDeath = new List<AudioClip>();
+    public List<AudioClip> clipsCollectLife = new List<AudioClip>();
+    public List<AudioClip> clipsCollision = new List<AudioClip>();
+    public List<AudioClip> clipsUseBoost = new List<AudioClip>();
+    public List<AudioClip> clipsReverseStream = new List<AudioClip>();
 
     //Pass parameters to coroutine
     private float _delay =  0.0f;
-    private bool loop = false;
+    private bool _loop = false;
     private soundType _type;
 
     public AudioSource source;
 
     public void PlayAudio(soundType type, bool loop = false, float delay = 0.0f)
     {
-        if( delay > 0.0f)
+        if (delay > 0.0f)
         {
             _type = type;
-            loop = loop;
+            _loop = loop;
             _delay = delay;
             StartCoroutine("PlayDelayedAudio");
         }
-        switch (type)
-        {
-            case soundType.far : PlayAudioFar(loop);
-                break;
-            case soundType.close : PlayAudioClose(loop);
-                break;
-            case soundType.receiveHit: PlayAudioReceiveHit(loop);
-                break;
-            case soundType.receiveStun: PlayAudioReceiveStun(loop);
-                break;
-            case soundType.enterOpenWorld:
-                PlayAudioEnterOpenWorld(loop);
-                break;
-            case soundType.collectFragment:
-                PlayAudioCollectFragment(loop);
-                break;
-        }
+        else
+            PlaySoundType(type, loop);
     }
 
-    public void PlaySpecificAudio(AudioClip clip)
+    public void PlaySpecificAudio(AudioClip clip, bool loop)
     {
         source.clip = clip;
         source.loop = loop;
@@ -65,116 +58,60 @@ public class AudioController : MonoBehaviour {
         source.Play();
     }
 
+    public void PlaySoundType(soundType type, bool loop)
+    {
+        switch (type)
+        {
+            case soundType.far:
+                playSoundFromList(clipsFar, loop);
+                break;
+            case soundType.close:
+                playSoundFromList(clipsClose, loop);
+                break;
+            case soundType.receiveHit:
+                playSoundFromList(clipsReceiveHit, loop);
+                break;
+            case soundType.receiveStun:
+                playSoundFromList(clipsReceiveStun, loop);
+                break;
+            case soundType.enterOpenWorld:
+                playSoundFromList(clipsEnterOpenWorld, loop);
+                break;
+            case soundType.collectFragment:
+                playSoundFromList(clipsCollectFragments, loop);
+                break;
+            case soundType.death:
+                playSoundFromList(clipsDeath, loop);
+                break;
+            case soundType.collectLife:
+                playSoundFromList(clipsCollectLife, loop);
+                break;
+            case soundType.collision:
+                playSoundFromList(clipsCollision, loop);
+                break;
+            case soundType.useBoost:
+                playSoundFromList(clipsUseBoost, loop);
+                break;
+            case soundType.reverseStream:
+                playSoundFromList(clipsReverseStream, loop);
+                break;
+            default: break;
+        }
+    }
+
     public IEnumerator PlayDelayedAudio()
     {
         yield return new WaitForSeconds(_delay);
-        switch (_type)
-        {
-            case soundType.far:
-                PlayAudioFar(loop);
-                break;
-            case soundType.close:
-                PlayAudioClose(loop);
-                break;
-            case soundType.receiveHit:
-                PlayAudioReceiveHit(loop);
-                break;
-            case soundType.receiveStun:
-                PlayAudioReceiveStun(loop);
-                break;
-            case soundType.enterOpenWorld:
-                PlayAudioEnterOpenWorld(loop);
-                break;
-            case soundType.collectFragment:
-                PlayAudioCollectFragment(loop);
-                break;
-        }
+        PlaySoundType(_type, _loop);
     }
 
-    public void PlayAudioFar(bool loop)
+    public void playSoundFromList(List<AudioClip> list, bool loop)
     {
-
-        if (clipsFar.Count > 0)
+        if (list.Count > 0)
         {
-            int index = Random.Range(0, clipsFar.Count);
+            int index = Random.Range(0, list.Count);
 
-            source.clip = clipsFar[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-
-    }
-
-	public void PlayAudioClose(bool loop) {
-        if (clipsClose.Count > 0)
-        {
-            int index = Random.Range(0, clipsClose.Count);
-
-            source.clip = clipsClose[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-	}
-
-    public void PlayAudioDeath(bool loop)
-    {
-        if (clipsClose.Count > 0)
-        {
-            int index = Random.Range(0, clipsClose.Count);
-
-            source.clip = clipsClose[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-    }
-
-    public void PlayAudioReceiveHit(bool loop)
-    {
-        if (clipsReceiveHit.Count > 0)
-        {
-            int index = Random.Range(0, clipsReceiveHit.Count);
-
-            source.clip = clipsReceiveHit[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-    }
-
-    public void PlayAudioReceiveStun(bool loop)
-    {
-        if (clipsReceiveStun.Count > 0)
-        {
-            int index = Random.Range(0, clipsReceiveStun.Count);
-
-            source.clip = clipsReceiveStun[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-    }
-    public void PlayAudioEnterOpenWorld(bool loop)
-    {
-        if (clipsEnterOpenWorld.Count > 0)
-        {
-            int index = Random.Range(0, clipsEnterOpenWorld.Count);
-
-            source.clip = clipsEnterOpenWorld[index];
-            source.loop = loop;
-
-            source.Play();
-        }
-    }
-    public void PlayAudioCollectFragment(bool loop)
-    {
-        if (clipsCollectFragments.Count > 0)
-        {
-            int index = Random.Range(0, clipsCollectFragments.Count);
-
-            source.clip = clipsCollectFragments[index];
+            source.clip = list[index];
             source.loop = loop;
 
             source.Play();
